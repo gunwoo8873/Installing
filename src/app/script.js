15bbb224482b23ed350f673c561fdbce2a2ca5eb
choigunwoo <gunwoo8873@outlook.kr>
2024-04-03 17:40:54 +0900

    Remove Files

--- src/app/script.js
+++
@@ -1,8 +0,0 @@
-// Data Timer
-const timerData = {
-    head_timer: function(){
-        const head_today = document.getElementById(`today__timer`)
-        return head_today.innerHTML = new Date().toLocaleTimeString()
-    }
-}
-setInterval(timerData.head_timer, 1000)
