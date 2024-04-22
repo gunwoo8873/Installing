96c954488db857f09c529c4970dfa01d9069bf25
choigunwoo <gunwoo8873@outlook.kr>
2024-04-22 15:53:45 +0900

    -

--- src/Devops/testcopy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# User Info
-echo User Name : ${NAME}
-echo Host Name : ${HOSTNAME}
-
-# $0 : File Name, $$ : Shell Process in Number
-echo File Name :  $0, $$ # ./guide.sh, 1826
-echo File Useing Sec : ${SECONDS} sec
