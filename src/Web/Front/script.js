cdd11ed9df0cb837898df953d3eb23eb7404b18c
choigunwoo <gunwoo8873@outlook.kr>
2024-05-06 14:02:13 +0900

    -

--- src/Web/Front/script.js
+++
@@ -1,9 +0,0 @@
-const menu = {
-    "head_menu": [
-        { "name": "HOME", "link": "index.html" },
-        { "name": "USER", "link": "about.html"},
-        { "name": "CONTENTS", "link": "contact.html" },
-        { "name": "GITHUB", "link": "contact.html" },
-        { "name": "DOCKERHUB", "link": "contact.html" },
-    ]
-}
